39c3db540dd87b9a1d8aa6b69b54c7dbf4225fea
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-04-06 11:29:17 +0200

    Remove unused functions in RequestUtil

--- src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
@@ -4,15 +4,7 @@ import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.util.pipeline.PipelineContext
 import net.logstash.logback.argument.StructuredArguments
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.concurrent.atomic.AtomicInteger
 
-const val NAV_CONSUMER_TOKEN = "Nav-Consumer-Token"
-const val TEMA =  "Tema"
-const val ALLE_TEMA_HEADERVERDI = "GEN"
-
-const val NAV_PERSONIDENT_HEADER = "Nav-Personident"
 const val MDC_CALL_ID = "callId"
 const val NAV_CALL_ID = "Nav-Call-Id"
 const val NAV_CALL_ID_HEADER = "X-Nav-CallId"
@@ -22,8 +14,3 @@ fun PipelineContext<out Unit, ApplicationCall>.getCallId(): String {
 }
 
 fun callIdArgument(callId: String) = StructuredArguments.keyValue("callId", callId)!!
-
-private val kafkaCounter = AtomicInteger(0)
-
-fun kafkaCallId(): String = "${LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-HHmm"))}-syfobrukertilgang-kafka-${kafkaCounter.incrementAndGet()}"
-
