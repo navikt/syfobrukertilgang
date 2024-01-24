4c528bb7a9696e79db9f751580470122981e0f11
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2024-01-24 15:04:45 +0100

    Remove constraints on dependencies that are no longer transitive dependencies (#67)

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,9 +91,7 @@ dependencies {
     api("io.ktor:ktor-client-mock-jvm:$ktorVersion")
 
     constraints {
-        implementation("org.eclipse.jetty:jetty-io:11.0.2")
         implementation("io.netty:netty-codec:4.1.106.Final")
-        implementation("net.minidev:json-smart:1.3.2")
     }
 }
 
