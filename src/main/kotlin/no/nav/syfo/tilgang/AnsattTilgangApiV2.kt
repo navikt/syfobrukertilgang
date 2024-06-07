6b1ae29ca1ae51f1113c67b28be38afd84d4234a
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2024-06-07 13:07:52 +0200

    Remove unused endpoint (#121)
    (cherry picked from commit fadba0b4451dd2ef38ac61f4b684ae3f209f0ae0)

--- src/main/kotlin/no/nav/syfo/tilgang/AnsattTilgangApiV2.kt
+++ src/main/kotlin/no/nav/syfo/tilgang/AnsattTilgangApiV2.kt
@@ -21,57 +21,6 @@ const val BASE_PATH_V2: String = "/api/v2/tilgang/ansatt"
 
 fun Route.registerAnsattTilgangApiV2(ansattTilgangService: AnsattTilgangService) {
     route(BASE_PATH_V2) {
-        get("/{fnr}") {
-            try {
-                val ansattFnr: String =
-                    call.parameters["fnr"]?.takeIf { validateFnr(it) } ?: throw IllegalArgumentException("Fnr mangler")
-
-                val credentials = call.principal<JWTPrincipal>()
-                val callId = getCallId()
-                credentials?.let { creds ->
-                    val pidClaim = creds.payload.getClaim(PID_CLAIM_NAME)
-                    if (pidClaim.isNull) {
-                        LOG.warn(
-                            "Mangler credentials for å authorisere bruker for tilgang til ansatt, {}, {}",
-                            callIdArgument(callId),
-                            consumerIdArgument(getConsumerId())
-                        )
-                        call.respond(
-                            HttpStatusCode.Unauthorized,
-                            "Kan ikke hente tilgang til ansatt: 'pid'-claim mangler i token fra id-porten"
-                        )
-                    } else {
-                        val loggedInFnr = pidClaim.asString()
-                        if (ansattTilgangService.hasAccessToAnsatt(loggedInFnr, ansattFnr)) {
-                            call.respond(true)
-                        } else {
-                            LOG.warn(
-                                "Innlogget bruker har ikke tilgang til oppslått ansatt, {}, {}",
-                                callIdArgument(callId),
-                                consumerIdArgument(getConsumerId())
-                            )
-                            call.respond(false)
-                        }
-                    }
-                } ?: run {
-                    LOG.warn(
-                        "Mangler credentials for å authorisere bruker for tilgang til ansatt, {}, {}",
-                        callIdArgument(callId),
-                        consumerIdArgument(getConsumerId())
-                    )
-                    call.respond(HttpStatusCode.Unauthorized, "Kan ikke hente tilgang til ansatt: Mangler credentials")
-                }
-            } catch (e: IllegalArgumentException) {
-                LOG.error(
-                    "Kan ikke hente tilgang til ansatt med fnr: {}, {}, {}",
-                    e.message,
-                    callIdArgument(getCallId()),
-                    consumerIdArgument(getConsumerId())
-                )
-                call.respond(HttpStatusCode.BadRequest, e.message ?: "Kan ikke hente tilgang til ansatt")
-            }
-        }
-
         get() {
             try {
                 val ansattFnr: String =
